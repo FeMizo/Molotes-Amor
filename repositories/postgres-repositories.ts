3610871b5818c56ff5da4e7a0f4f3fce7f13f0f4
FeMizo <femiss0693@gmail.com>
2026-03-17 20:45:45 -0600

    :hammer: V1.8.1

--- repositories/postgres-repositories.ts
+++ repositories/postgres-repositories.ts
@@ -415,7 +415,7 @@ export const postgresOrderRepository: OrderRepository = {
               payment_clabe,
               notes
             ) VALUES (
-              $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17
+              $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18
             )
           `,
           [
